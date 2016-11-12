58b164b178414facc34d883fd485920ff51ed03a
Gabriel Klockner <gabriel.klockner@gmail.com>
2016-11-12 13:40:57 -0200

    First commit, bot responding to hello

---
+++ python_bot.py
@@ -0,0 +1,78 @@
+import os
+import time
+from slackclient import SlackClient
+
+# Bot's ID as an environment variable
+BOT_ID = os.environ.get("BOT_ID")
+
+# constants
+AT_BOT = "<@" + BOT_ID + ">"
+EXAMPLE_COMMAND = "/"
+
+# instantiate Slack & Twilio clients
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+def get_users():
+    api_call = slack_client.api_call("users.list")
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        return api_call.get('members')
+
+def find_user(user_id):
+    users = get_users()
+    for user in users:
+        if user_id in user['id']:
+            return user['real_name']
+
+def greetings(user_id):
+    users = get_users()
+    for user in users:
+        if user_id in user['id']:
+            if user['is_admin']:
+                response = "Hello my lord " + find_user(user_id) + ", welcome sir :>"
+            else:
+                response = "Hello " + find_user(user_id) + ", welcome :)"
+            return response
+
+def handle_command(command, channel, user_id):
+    """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If so, then acts on the commands. If not,
+        returns back what it needs for clarification.
+    """
+    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
+               "* command."
+    if command.startswith(EXAMPLE_COMMAND):
+        response = "Sure...write some more code then I can do that!"
+    elif command.lower() == 'hello':
+        response = greetings(user_id)
+
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+
+def parse_slack_output(slack_rtm_output):
+    """
+        The Slack Real Time Messaging API is an events firehose.
+        this parsing function returns None unless a message is
+        directed at the Bot, based on its ID.
+    """
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @ mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                       output['channel'], output['user']
+    return None, None, None
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("Bot connected and running!")
+        while True:
+            command, channel, user_id = parse_slack_output(slack_client.rtm_read())
+            if command and channel and user_id:
+                handle_command(command, channel, user_id)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
